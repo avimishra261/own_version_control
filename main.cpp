e031a513feb5c697f2846dcad0536695de2c0ec8
Avi Mishra <avimishra261@gmail.com>
2024-06-13 23:14:13 +0530

    feat: cat-file -p command added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include "zstr.hpp"
 
 int main(int argc, char * argv[]){
     if(argc < 2){
@@ -30,6 +31,32 @@ int main(int argc, char * argv[]){
             std::cerr << e.what() << '\n';
             return EXIT_FAILURE;
         }
+    }else if(command == "cat-file"){
+        if(argc <= 3){
+            std::cerr << "Invalid arguments, required '-p <blob_sha>'\n";
+            return EXIT_FAILURE;
+        }
+
+        const std::string flag = argv[2]; //  Getting flag like -p for cat-file command
+        if(flag != "-p"){
+            std::cerr << "Invalid flag for cat-file, expected '-p' flag\n";
+            return EXIT_FAILURE;
+        }
+        const std::string value = argv[3]; // Getting the sha value of blob
+        const std::string dir_name = value.substr(0,2); // In git dir is made up of starting 2 chars of sha value
+        const std::string blob_sha = value.substr(2); // Rest of the chars in sha becomes the file name in the dir
+
+        std::string path = ".git/objects" + dir_name + "/" + blob_sha;
+        zstr::ifstream input(path, std::ofstream::binary);
+        if(!input.is_open()){
+            std::cerr << "Failed to open the object file\n";
+            return EXIT_FAILURE;
+        }
+        std::string object_str(std::istreambuf_iterator<char>(input),std::istreambuf_iterator<char>());
+        input.close();
+        const auto object_content = object_str.substr(object_str.find('\0')+1);
+        std::cout << object_content << std::flush;
+
     }else{
         std::cerr << "Unknown command " << command << '\n';
         return EXIT_FAILURE;
